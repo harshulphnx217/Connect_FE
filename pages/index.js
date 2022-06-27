78ae0b0ede02f54d840ad9cdd46f1b8dd1e80269
CREATORS <harshulwalia217@gmail.com>
2022-06-27 21:41:09 +0530

    LOGIN INTEGRATION

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,58 @@
 import Head from 'next/head'
+<<<<<<< Updated upstream
 import styles from '../styles/Home.module.css'
 import Footer from '../components/footer'
+=======
+import styles from '../styles/index.module.css'
+import Link from 'next/link';
+import Register from './register';
+import Profile from './profile';
+export default function Index() {
 
-export default function Home() {
+   
+    function login(e) {
+        e.preventDefault();
+        const data = {
+            phone_number: document.getElementById("phno").value,
+            password: document.getElementById("pswrd").value
+        }
+        const formData = new FormData();
+        // Checking if all entries are not null
+        if(data.phone_number !== '' && data.password !== ''){
+            formData.append("phone_number", data.phone_number)
+            formData.append("password",data.password)
+
+            fetch("https://connect-api-social.herokuapp.com/user/login", {
+            method: "POST",
+            body: formData
+        })
+            .then(res => res.json())
+            .then(res => {
+                if (res.status === 200){
+                    // Saved apikey to website cookies/sharedPrefs
+                    console.log("SUCCES")
+                    localStorage.setItem("api_key",res.result.api_key);
+                    // console.log(localStorage.getItem("api_key"))
+                    // <Navigate to='/Home'/>     
+                   <Link href={Profile}/>
+                    // TODO::Redirection to Home Screen on Success Response
+                     
+                }
+                else{
+                    console.log(res.result)
+                    alert(res.result)
+                }
+            })
+            .catch(err => console.log(err));
+        }
+        else{
+            alert("Please enter your login details properly")
+        }
+    }
+>>>>>>> Stashed changes
+
   return (
+
     <div >
       <Head className={styles.container}>
         <title>Connect</title>
@@ -15,6 +64,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <div className={styles.grid}>
+<<<<<<< Updated upstream
           <h2 className={styles.card}><img src="/connectlogo.svg" alt=" CONNECT " /></h2>        
           <div className={styles.cardLogin} style={{border: "1px solid blue"}}>
             <form method='post'>
@@ -26,6 +76,17 @@ export default function Home() {
             </form>
             <button className={styles.regbtn} id='regbtn' name='Register' onClick={goToRegister}>Register</button>   
           </div>
+=======
+            <h2 className={styles.card}><img src="/connectlogo.svg" alt=" CONNECT " /></h2>        
+            <form className={styles.cardLogin} style={{border: "1px solid blue"}} >
+                <h2>Login</h2>
+                <input className={styles.inp}type="tel" id='phno' name='Phone Number' placeholder='Phone Number'/>
+                <input className={styles.inp}type="password" id='pswrd' name='Phone Number' placeholder='Password'/>
+                <a href='forgotPassword'>Forgot Password?</a>
+                <button className={styles.lgnbtn} id='lgnbtn' name='Login' onClick={login}>Login</button>   
+                <button className={styles.regbtn} id='regbtn' name='Register'><Link href="Register"><a>Register</a></Link></button>   
+            </form>       
+>>>>>>> Stashed changes
         </div>
         <Footer/>
       </main>

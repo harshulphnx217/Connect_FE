427a39058041cfa0f5bd4a0cf8d53ffa5898c4ed
Blake2912 <kcvarun29122001@gmail.com>
2022-06-28 23:56:28 +0530

    Added Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,41 @@
 import styles from '../styles/navbar.module.css'
 export default function NavBar(){
     return(
-        <div className={styles.main}>
-            
+        <div class="navbar" id={styles.nav_bar}>
+            <div class="flex-1">
+                <a class="btn btn-square btn-ghost" id={styles.logo}><img src='/connect_nav.png'/></a>
+            </div>
+            <div class="flex-none">
+                <div class="dropdown dropdown-end" id={styles.friends_icon}>
+                <label tabindex="0" class="btn btn-ghost btn-circle">
+                    <div class="indicator">
+                    <img className={styles.icons} src="/friends 1.svg" alt=" EDIT PROFILE " />
+                    <span class="badge badge-sm indicator-item">8</span>
+                    </div>
+                </label>
+                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>My Friends</a></li>
+                    <li><a>Find Friends</a></li>
+                </ul>
+                </div>
+                <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                    <img src="https://api.lorem.space/image/face?hash=33791" />
+                    </div>
+                </label>
+                <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li>
+                    <a class="justify-between">
+                        Profile
+                        <span class="badge">New</span>
+                    </a>
+                    </li>
+                    <li><a>Settings</a></li>
+                    <li><a>Logout</a></li>
+                </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,13 @@
 import NavBar from '../components/navbar'
 import Post from '../components/post'
+import Footer from '../components/footer'
 import styles from '../styles/home.module.css'
 export default function Home(){
     return(
         <main>
             <NavBar/>
             <Post/>
+            <Footer/>
         </main>
     )
 }

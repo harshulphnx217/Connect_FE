8a83bad29677bd9dcebe5cf0dde43bbb02231d97
Yogish M <yogishmohan2000@gmail.com>
2022-06-30 21:41:23 +0530

    comment component ui designed and added to home page

--- components/post.js
+++ components/post.js
@@ -1,8 +1,20 @@
 import styles from '../styles/post.module.css'
 export default function Post(){
     return(
-        <div>
-            <p>This is the post component!</p>
+        <div className={styles.postContainer}>
+             <div className={styles.postLeft}>
+             <img className={styles.profilepic} src="/profileicon.svg" alt=" PROFILE " />
+             <p id='username'>Vathi Coming</p>
+             <h3 className={styles.postHeading}>Post Heading</h3>
+            
+             <img className={styles.postpic} src="/selfie.svg" alt=" POSTPIC " />
+             <input className={styles.likeBtn}type="image" id='likeBtn' name='likeBtn' src="/likeBtn.svg"/>
+             {/* <p id='likeCounter'>123 likes</p> */}
+             <input className={styles.commentBtn}type="image" id='commentBtn' name='commentBtn' src="/commentBtn.svg"/>   
+             {/* <p className={styles.commentBtn}>100</p> */}
+            </div>
+
+            <p className={styles.postDescription}>Post Description</p>
         </div>
     )
 }
